b28cc3b4b2b9054b1afe6178a762301bf287d247
Hao1997 <hluo072@uottawa.ca>
2020-01-02 13:09:45 -0500

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { All_Projects } from './data/all_projects';
+import { All_Personal } from './data/all_personal';
+import { Project } from './model/project'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'This is section';
+  detail = `They got there early, and they got really good seats.
+  She works two jobs to make ends meet; at least, that was her reason for not having time to join us.
+I am happy to take your donation; any amount will be greatly appreciated.
+She advised him to come back at once.
+We need to rent a room for our party.`;
+  myProjects = All_Projects;
+  myPersonal = All_Personal;
+
+  constructor() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HeaderModule } from './components/header/header.module'
+import { FooterModule } from './components/footer/footer.module'
+
+
+import { HomePageModule } from './pages/home/home.module'
+import { ProjectPageModule } from './pages/project/project.module'
+import { LifePageModule } from './pages/life/life.module'
+import { ResumePageModule } from './pages/resume/resume.module'
+import { ContactPageModule } from './pages/contact/contact.module'
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppRoutingModule,
+    HeaderModule,
+    FooterModule,
+    HomePageModule,
+    ProjectPageModule,
+    LifePageModule,
+    ResumePageModule,
+    ContactPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
